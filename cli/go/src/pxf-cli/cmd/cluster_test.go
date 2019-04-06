31b41100d4d9a4af94724e96dbe6f168db1504f2
Oliver Albertini <oalbertini@pivotal.io>
2019-04-06 10:31:16 -0700

    Clean up tests
    Mostly for ease of readability. Also takes out variables like
    `expectedError` and just uses strings (much easier to understand test
    failures that way, thanks @benchristel for the tip!).
    
    Authored-by: Oliver Albertini <oalbertini@pivotal.io>

--- cli/go/src/pxf-cli/cmd/cluster_test.go
+++ cli/go/src/pxf-cli/cmd/cluster_test.go
@@ -25,8 +25,8 @@ var (
 	}
 )
 
-var _ = Describe("GenerateHostList", func() {
-	It("Returns the correct hostlist and master hostname", func() {
+var _ = Describe("CountHostsAndValidateMaster()", func() {
+	It("calculates the correct number of hosts when run from master", func() {
 		operating.System.Hostname = func() (string, error) {
 			return "mdw", nil
 		}
@@ -34,9 +34,9 @@ var _ = Describe("GenerateHostList", func() {
 		Expect(err).To(BeNil())
 		Expect(clusterData.NumHosts).To(Equal(2))
 	})
-	It("Errors if not running from master host", func() {
+	It("returns an error if not run from master host; number of hosts set to -1", func() {
 		operating.System.Hostname = func() (string, error) {
-			return "fake-host", nil
+			return "wrong-hostname", nil
 		}
 		err := clusterData.CountHostsAndValidateMaster()
 		Expect(err.Error()).To(Equal("ERROR: pxf cluster commands should only be run from Greenplum master"))
@@ -44,7 +44,32 @@ var _ = Describe("GenerateHostList", func() {
 	})
 })
 
-var _ = Describe("GenerateOutput", func() {
+var _ = Describe("GenerateStatusReport()", func() {
+	BeforeEach(func() {
+		clusterData.NumHosts = 2
+	})
+	It("reports the number of hosts that will be initialized", func() {
+		_ = cmd.GenerateStatusReport(&pxf.Init, clusterData)
+		Expect(testStdout).Should(gbytes.Say("Initializing PXF on master and 2 segment hosts..."))
+	})
+
+	It("reports the number of hosts that will be started", func() {
+		_ = cmd.GenerateStatusReport(&pxf.Start, clusterData)
+		Expect(testStdout).Should(gbytes.Say("Starting PXF on 2 segment hosts..."))
+	})
+
+	It("reports the number of hosts that will be stopped", func() {
+		_ = cmd.GenerateStatusReport(&pxf.Stop, clusterData)
+		Expect(testStdout).Should(gbytes.Say("Stopping PXF on 2 segment hosts..."))
+	})
+
+	It("reports the number of hosts that will be synced", func() {
+		_ = cmd.GenerateStatusReport(&pxf.Sync, clusterData)
+		Expect(testStdout).Should(gbytes.Say("Syncing PXF configuration files to 2 hosts..."))
+	})
+})
+
+var _ = Describe("GenerateOutput()", func() {
 	BeforeEach(func() {
 		clusterData.Output = &cluster.RemoteOutput{
 			NumErrors: 0,
@@ -53,135 +78,105 @@ var _ = Describe("GenerateOutput", func() {
 			Errors:    map[int]error{-1: nil, 0: nil, 1: nil},
 		}
 	})
-	Describe("Running supported commands", func() {
-		Context("When all hosts are successful", func() {
-			It("Reports all hosts initialized successfully", func() {
-				_ = cmd.GenerateOutput(&pxf.Init, clusterData)
-				Expect(testStdout).To(gbytes.Say("PXF initialized successfully on 3 out of 3 hosts"))
-			})
-
-			It("Reports all hosts started successfully", func() {
-				_ = cmd.GenerateOutput(&pxf.Start, clusterData)
-				Expect(testStdout).To(gbytes.Say("PXF started successfully on 3 out of 3 hosts"))
-			})
-
-			It("Reports all hosts stopped successfully", func() {
-				_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
-				Expect(testStdout).To(gbytes.Say("PXF stopped successfully on 3 out of 3 hosts"))
-			})
-
-			It("Reports all hosts synced successfully", func() {
-				_ = cmd.GenerateOutput(&pxf.Sync, clusterData)
-				Expect(testStdout).To(gbytes.Say("PXF configs synced successfully on 3 out of 3 hosts"))
-			})
+	Context("when all hosts are successful", func() {
+		It("reports all hosts initialized successfully", func() {
+			_ = cmd.GenerateOutput(&pxf.Init, clusterData)
+			Expect(testStdout).To(gbytes.Say("PXF initialized successfully on 3 out of 3 hosts"))
 		})
 
-		Context("When some hosts fail", func() {
-			var expectedError string
-			BeforeEach(func() {
-				clusterData.Output = &cluster.RemoteOutput{
-					NumErrors: 1,
-					Stderrs:   map[int]string{-1: "", 0: "", 1: "an error happened on sdw2"},
-					Stdouts:   map[int]string{-1: "everything fine", 0: "everything fine", 1: "something wrong"},
-					Errors:    map[int]error{-1: nil, 0: nil, 1: errors.New("some error")},
-				}
-				expectedError = "sdw2 ==> an error happened on sdw2"
-			})
-			It("Reports the number of hosts that failed to initialize", func() {
-				_ = cmd.GenerateOutput(&pxf.Init, clusterData)
-				Expect(testStdout).Should(gbytes.Say("PXF failed to initialize on 1 out of 3 hosts"))
-				Expect(testStderr).Should(gbytes.Say(expectedError))
-			})
-
-			It("Reports the number of hosts that failed to start", func() {
-				_ = cmd.GenerateOutput(&pxf.Start, clusterData)
-				Expect(testStdout).Should(gbytes.Say("PXF failed to start on 1 out of 3 hosts"))
-				Expect(testStderr).Should(gbytes.Say(expectedError))
-			})
-
-			It("Reports the number of hosts that failed to stop", func() {
-				_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
-				Expect(testStdout).Should(gbytes.Say("PXF failed to stop on 1 out of 3 hosts"))
-				Expect(testStderr).Should(gbytes.Say(expectedError))
-			})
-
-			It("Reports the number of hosts that failed to sync", func() {
-				_ = cmd.GenerateOutput(&pxf.Sync, clusterData)
-				Expect(testStdout).Should(gbytes.Say("PXF configs failed to sync on 1 out of 3 hosts"))
-				Expect(testStderr).Should(gbytes.Say(expectedError))
-			})
+		It("reports all hosts started successfully", func() {
+			_ = cmd.GenerateOutput(&pxf.Start, clusterData)
+			Expect(testStdout).To(gbytes.Say("PXF started successfully on 3 out of 3 hosts"))
 		})
 
-		Context("Before the command returns", func() {
-			BeforeEach(func() {
-				clusterData.NumHosts = 2
-			})
-			It("Reports the number of hosts that will be initialized", func() {
-				_ = cmd.GenerateStatusReport(&pxf.Init, clusterData)
-				Expect(testStdout).Should(gbytes.Say("Initializing PXF on master and 2 segment hosts..."))
-			})
-
-			It("Reports the number of hosts will be started", func() {
-				_ = cmd.GenerateStatusReport(&pxf.Start, clusterData)
-				Expect(testStdout).Should(gbytes.Say("Starting PXF on 2 segment hosts..."))
-			})
-
-			It("Reports the number of hosts that will be stopped", func() {
-				_ = cmd.GenerateStatusReport(&pxf.Stop, clusterData)
-				Expect(testStdout).Should(gbytes.Say("Stopping PXF on 2 segment hosts..."))
-			})
-
-			It("Reports the number of hosts that will be synced", func() {
-				_ = cmd.GenerateStatusReport(&pxf.Sync, clusterData)
-				Expect(testStdout).Should(gbytes.Say("Syncing PXF configuration files to 2 hosts..."))
-			})
+		It("reports all hosts stopped successfully", func() {
+			_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
+			Expect(testStdout).To(gbytes.Say("PXF stopped successfully on 3 out of 3 hosts"))
 		})
 
-		Context("When we see messages in Stderr, but NumErrors is 0", func() {
-			It("Reports all hosts were successful", func() {
-				clusterData.Output = &cluster.RemoteOutput{
-					NumErrors: 0,
-					Stderrs:   map[int]string{-1: "typical stderr", 0: "typical stderr", 1: "typical stderr"},
-					Stdouts:   map[int]string{-1: "typical stdout", 0: "typical stdout", 1: "typical stdout"},
-					Errors:    map[int]error{-1: nil, 0: nil, 1: nil},
-				}
-				_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
-				Expect(testStdout).To(gbytes.Say("PXF stopped successfully on 3 out of 3 hosts"))
-			})
+		It("reports all hosts synced successfully", func() {
+			_ = cmd.GenerateOutput(&pxf.Sync, clusterData)
+			Expect(testStdout).To(gbytes.Say("PXF configs synced successfully on 3 out of 3 hosts"))
+		})
+	})
+
+	Context("when some hosts fail", func() {
+		BeforeEach(func() {
+			clusterData.Output = &cluster.RemoteOutput{
+				NumErrors: 1,
+				Stderrs:   map[int]string{-1: "", 0: "", 1: "an error happened on sdw2"},
+				Stdouts:   map[int]string{-1: "everything fine", 0: "everything fine", 1: "something wrong"},
+				Errors:    map[int]error{-1: nil, 0: nil, 1: errors.New("some error")},
+			}
+		})
+		It("reports the number of hosts that failed to initialize", func() {
+			_ = cmd.GenerateOutput(&pxf.Init, clusterData)
+			Expect(testStdout).Should(gbytes.Say("PXF failed to initialize on 1 out of 3 hosts"))
+			Expect(testStderr).Should(gbytes.Say("sdw2 ==> an error happened on sdw2"))
 		})
 
-		Context("When a command fails, and output is multiline", func() {
-			It("Truncates the output to two lines", func() {
-				stderr := `stderr line one
+		It("reports the number of hosts that failed to start", func() {
+			_ = cmd.GenerateOutput(&pxf.Start, clusterData)
+			Expect(testStdout).Should(gbytes.Say("PXF failed to start on 1 out of 3 hosts"))
+			Expect(testStderr).Should(gbytes.Say("sdw2 ==> an error happened on sdw2"))
+		})
+
+		It("reports the number of hosts that failed to stop", func() {
+			_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
+			Expect(testStdout).Should(gbytes.Say("PXF failed to stop on 1 out of 3 hosts"))
+			Expect(testStderr).Should(gbytes.Say("sdw2 ==> an error happened on sdw2"))
+
+		})
+
+		It("reports the number of hosts that failed to sync", func() {
+			_ = cmd.GenerateOutput(&pxf.Sync, clusterData)
+			Expect(testStdout).Should(gbytes.Say("PXF configs failed to sync on 1 out of 3 hosts"))
+			Expect(testStderr).Should(gbytes.Say("sdw2 ==> an error happened on sdw2"))
+
+		})
+	})
+
+	Context("when we see messages in Stderr, but NumErrors is 0", func() {
+		It("reports all hosts were successful", func() {
+			clusterData.Output = &cluster.RemoteOutput{
+				NumErrors: 0,
+				Stderrs:   map[int]string{-1: "typical stderr", 0: "typical stderr", 1: "typical stderr"},
+				Stdouts:   map[int]string{-1: "typical stdout", 0: "typical stdout", 1: "typical stdout"},
+				Errors:    map[int]error{-1: nil, 0: nil, 1: nil},
+			}
+			_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
+			Expect(testStdout).To(gbytes.Say("PXF stopped successfully on 3 out of 3 hosts"))
+		})
+	})
+
+	Context("when a command fails, and output is multiline", func() {
+		It("truncates the output to two lines", func() {
+			stderr := `stderr line one
 stderr line two
 stderr line three`
-				clusterData.Output = &cluster.RemoteOutput{
-					NumErrors: 1,
-					Stderrs:   map[int]string{-1: "", 0: "", 1: stderr},
-					Stdouts:   map[int]string{-1: "everything fine", 0: "everything fine", 1: "everything not fine"},
-					Errors:    map[int]error{-1: nil, 0: nil, 1: errors.New("some error")},
-				}
-				expectedError := `sdw2 ==> stderr line one
-stderr line two...`
-				_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
-				Expect(testStdout).Should(gbytes.Say(fmt.Sprintf("PXF failed to stop on 1 out of 3 hosts")))
-				Expect(testStderr).Should(gbytes.Say(expectedError))
-			})
+			clusterData.Output = &cluster.RemoteOutput{
+				NumErrors: 1,
+				Stderrs:   map[int]string{-1: "", 0: "", 1: stderr},
+				Stdouts:   map[int]string{-1: "everything fine", 0: "everything fine", 1: "everything not fine"},
+				Errors:    map[int]error{-1: nil, 0: nil, 1: errors.New("some error")},
+			}
+			_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
+			Expect(testStdout).Should(gbytes.Say(fmt.Sprintf("PXF failed to stop on 1 out of 3 hosts")))
+			Expect(testStderr).Should(gbytes.Say("sdw2 ==> stderr line one\nstderr line two..."))
 		})
+	})
 
-		Context("When NumErrors is non-zero, but Stderr is empty", func() {
-			It("Reports Stdout in error message", func() {
-				clusterData.Output = &cluster.RemoteOutput{
-					NumErrors: 1,
-					Stderrs:   map[int]string{-1: "", 0: "", 1: ""},
-					Stdouts:   map[int]string{-1: "everything fine", 0: "everything fine", 1: "something wrong on sdw2\nstderr line2\nstderr line3"},
-					Errors:    map[int]error{-1: nil, 0: nil, 1: errors.New("some error")},
-				}
-				expectedError := "sdw2 ==> something wrong on sdw2\nstderr line2..."
-				_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
-				Expect(testStdout).Should(gbytes.Say("PXF failed to stop on 1 out of 3 hosts"))
-				Expect(testStderr).Should(gbytes.Say(expectedError))
-			})
+	Context("when NumErrors is non-zero, but Stderr is empty", func() {
+		It("reports Stdout in error message", func() {
+			clusterData.Output = &cluster.RemoteOutput{
+				NumErrors: 1,
+				Stderrs:   map[int]string{-1: "", 0: "", 1: ""},
+				Stdouts:   map[int]string{-1: "everything fine", 0: "everything fine", 1: "something wrong on sdw2\nstderr line2\nstderr line3"},
+				Errors:    map[int]error{-1: nil, 0: nil, 1: errors.New("some error")},
+			}
+			_ = cmd.GenerateOutput(&pxf.Stop, clusterData)
+			Expect(testStdout).Should(gbytes.Say("PXF failed to stop on 1 out of 3 hosts"))
+			Expect(testStderr).Should(gbytes.Say("sdw2 ==> something wrong on sdw2\nstderr line2..."))
 		})
 	})
 })
--- cli/go/src/pxf-cli/pxf/pxf_test.go
+++ cli/go/src/pxf-cli/pxf/pxf_test.go
@@ -10,77 +10,114 @@ import (
 )
 
 var _ = Describe("CommandFunc", func() {
-	BeforeEach(func() {
-		_ = os.Setenv("GPHOME", "/test/gphome")
-		_ = os.Setenv("PXF_CONF", "/test/gphome/pxf_conf")
+	Context("when PXF_CONF and GPHOME are set", func() {
+		BeforeEach(func() {
+			_ = os.Setenv("GPHOME", "/test/gphome")
+			_ = os.Setenv("PXF_CONF", "/test/gphome/pxf_conf")
+		})
+
+		It("successfully generates init command", func() {
+			commandFunc, err := pxf.Init.GetFunctionToExecute()
+			Expect(err).To(BeNil())
+			expected := "PXF_CONF=/test/gphome/pxf_conf /test/gphome/pxf/bin/pxf init"
+			Expect(commandFunc("foo")).To(Equal(expected))
+		})
 	})
 
-	It("Is successful when GPHOME and PXF_CONF are set and init is called", func() {
-		commandFunc, err := pxf.Init.GetFunctionToExecute()
-		Expect(err).To(BeNil())
-		expected := "PXF_CONF=/test/gphome/pxf_conf /test/gphome/pxf/bin/pxf init"
-		Expect(commandFunc("foo")).To(Equal(expected))
+	Context("when only GPHOME is set", func() {
+		BeforeEach(func() {
+			_ = os.Setenv("GPHOME", "/test/gphome")
+			_ = os.Unsetenv("PXF_CONF")
+		})
+
+		It("is successful when GPHOME is set and start/stop are called", func() {
+			commandFunc, err := pxf.Start.GetFunctionToExecute()
+			Expect(err).To(BeNil())
+			Expect(commandFunc("foo")).To(Equal("/test/gphome/pxf/bin/pxf start"))
+			commandFunc, err = pxf.Stop.GetFunctionToExecute()
+			Expect(err).To(BeNil())
+			Expect(commandFunc("foo")).To(Equal("/test/gphome/pxf/bin/pxf stop"))
+		})
+		It("fails to init or sync", func() {
+			commandFunc, err := pxf.Init.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("PXF_CONF must be set")))
+			commandFunc, err = pxf.Sync.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("PXF_CONF must be set")))
+		})
+
+	})
+	Context("when only PXF_CONF is set", func() {
+		BeforeEach(func() {
+			_ = os.Setenv("PXF_CONF", "/test/gphome/pxf_conf")
+			_ = os.Unsetenv("GPHOME")
+		})
+
+		It("sets up rsync commands of $PXF_CONF/{conf,lib,servers} dirs", func() {
+			commandFunc, err := pxf.Sync.GetFunctionToExecute()
+			Expect(err).To(BeNil())
+			Expect(commandFunc("sdw1")).To(Equal(
+				"rsync -az -e 'ssh -o StrictHostKeyChecking=no' " +
+					"'/test/gphome/pxf_conf/conf' " +
+					"'/test/gphome/pxf_conf/lib' " +
+					"'/test/gphome/pxf_conf/servers' " +
+					"'sdw1:/test/gphome/pxf_conf'",
+			))
+			Expect(commandFunc("sdw2")).To(Equal(
+				"rsync -az -e 'ssh -o StrictHostKeyChecking=no' " +
+					"'/test/gphome/pxf_conf/conf' " +
+					"'/test/gphome/pxf_conf/lib' " +
+					"'/test/gphome/pxf_conf/servers' " +
+					"'sdw2:/test/gphome/pxf_conf'",
+			))
+		})
+
+		It("fails to init, start, or stop", func() {
+			commandFunc, err := pxf.Init.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("GPHOME must be set")))
+			commandFunc, err = pxf.Start.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("GPHOME must be set")))
+			commandFunc, err = pxf.Stop.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("GPHOME must be set")))
+		})
 	})
 
-	It("Is successful when GPHOME is set and start/stop are called", func() {
-		commandFunc, err := pxf.Start.GetFunctionToExecute()
-		Expect(err).To(BeNil())
-		Expect(commandFunc("foo")).To(Equal("/test/gphome/pxf/bin/pxf start"))
-		commandFunc, err = pxf.Stop.GetFunctionToExecute()
-		Expect(err).To(BeNil())
-		Expect(commandFunc("foo")).To(Equal("/test/gphome/pxf/bin/pxf stop"))
+	Context("when PXF_CONF is set to empty string", func() {
+		BeforeEach(func() {
+			_ = os.Setenv("PXF_CONF", "")
+			_ = os.Setenv("GPHOME", "/test/gphome")
+		})
+		It("fails to init or sync", func() {
+			_ = os.Setenv("PXF_CONF", "")
+			commandFunc, err := pxf.Init.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("PXF_CONF cannot be blank")))
+			commandFunc, err = pxf.Sync.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("PXF_CONF cannot be blank")))
+		})
 	})
 
-	It("Fails to init or sync when PXF_CONF is not set", func() {
-		_ = os.Unsetenv("PXF_CONF")
-		commandFunc, err := pxf.Init.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("PXF_CONF must be set")))
-		commandFunc, err = pxf.Sync.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("PXF_CONF must be set")))
-	})
-
-	It("Fails to init or sync when PXF_CONF is blank", func() {
-		_ = os.Setenv("PXF_CONF", "")
-		commandFunc, err := pxf.Init.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("PXF_CONF cannot be blank")))
-		commandFunc, err = pxf.Sync.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("PXF_CONF cannot be blank")))
-	})
-
-	It("Fails to init, start, or stop when GPHOME is not set", func() {
-		_ = os.Unsetenv("GPHOME")
-		commandFunc, err := pxf.Init.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("GPHOME must be set")))
-		commandFunc, err = pxf.Start.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("GPHOME must be set")))
-		commandFunc, err = pxf.Stop.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("GPHOME must be set")))
-	})
-
-	It("Fails to init, start, or stop when GPHOME is blank", func() {
-		_ = os.Setenv("GPHOME", "")
-		commandFunc, err := pxf.Init.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("GPHOME cannot be blank")))
-		commandFunc, err = pxf.Start.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("GPHOME cannot be blank")))
-		commandFunc, err = pxf.Stop.GetFunctionToExecute()
-		Expect(commandFunc).To(BeNil())
-		Expect(err).To(Equal(errors.New("GPHOME cannot be blank")))
-	})
-
-	It("sets up rsync commands of $PXF_CONF/{conf,lib,servers}", func() {
-		commandFunc, err := pxf.Sync.GetFunctionToExecute()
-		Expect(err).To(BeNil())
-		Expect(commandFunc("sdw1")).To(Equal("rsync -az -e 'ssh -o StrictHostKeyChecking=no' '/test/gphome/pxf_conf/conf' '/test/gphome/pxf_conf/lib' '/test/gphome/pxf_conf/servers' 'sdw1:/test/gphome/pxf_conf'"))
-		Expect(commandFunc("sdw2")).To(Equal("rsync -az -e 'ssh -o StrictHostKeyChecking=no' '/test/gphome/pxf_conf/conf' '/test/gphome/pxf_conf/lib' '/test/gphome/pxf_conf/servers' 'sdw2:/test/gphome/pxf_conf'"))
+	Context("when GPHOME is set to empty string", func() {
+		BeforeEach(func() {
+			_ = os.Setenv("GPHOME", "")
+			_ = os.Unsetenv("PXF_CONF")
+		})
+		It("fails to init, start, or stop", func() {
+			_ = os.Setenv("GPHOME", "")
+			commandFunc, err := pxf.Init.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("GPHOME cannot be blank")))
+			commandFunc, err = pxf.Start.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("GPHOME cannot be blank")))
+			commandFunc, err = pxf.Stop.GetFunctionToExecute()
+			Expect(commandFunc).To(BeNil())
+			Expect(err).To(Equal(errors.New("GPHOME cannot be blank")))
+		})
 	})
 })
