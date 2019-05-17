fcf0b06b178931956507626c28312735e7bcf506
Shivram Mani <smani@pivotal.io>
2019-05-17 16:51:50 -0700

    8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk8196 Hdfs line vs chunk
    Authored-by: Shivram Mani <smani@pivotal.io>

--- automation/src/test/java/org/greenplum/pxf/automation/performance/PerformanceTest.java
+++ automation/src/test/java/org/greenplum/pxf/automation/performance/PerformanceTest.java
@@ -30,7 +30,7 @@ import org.greenplum.pxf.automation.features.BaseFeature;
 public class PerformanceTest extends BaseFeature {
 
     private static final String GENERATE_TEXT_DATA_COL_DELIMITER = ",";
-    private static final long GENERATE_TEXT_DATA_SIZE_MB = 32768;
+    private static final long GENERATE_TEXT_DATA_SIZE_MB = 8196;
     private static final int GENERATE_COLUMN_MAX_WIDTH = 50;
     private static final int GENERATE_INT_COLUMNS_NUMBER = 5;
     private static final int GENERATE_TEXT_COLUMNS_NUMBER = 5;
