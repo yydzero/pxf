cda684d62ffdd244af5b10f60b65e4991292085e
Oliver Albertini <oalbertini@pivotal.io>
2019-12-07 17:15:54 -0800

    Pre-compute the size of StringBuilder required
    That way we can avoid calls to arrayCopy().
    
    Authored-by: Oliver Albertini <oalbertini@pivotal.io>

--- server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/ImageResolver.java
+++ server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/ImageResolver.java
@@ -20,58 +20,27 @@ public class ImageResolver extends BasePlugin implements BatchResolver {
     private int currentImage;
     List<InputStream> inputStreams;
     // cache of strings for RGB arrays going to Greenplum
-    private static String[] reds = new String[256];
-    private static String[] greens = new String[256];
-    private static String[] blues = new String[256];
+    private static String[] r = new String[256];
+    private static String[] g = new String[256];
+    private static String[] b = new String[256];
 
     static {
         for (int i = 0; i < 256; i++) {
-            reds[i] = "{" + i;
-            greens[i] = "," + i + ",";
-            blues[i] = i + "}";
+            r[i] = "{" + i;
+            g[i] = "," + i + ",";
+            b[i] = i + "}";
         }
+    }
+
+
+    @Override
+    public List<OneField> getFields(OneRow row) {
+        return null;
     }
 
     /**
      * Returns a Postgres-style array with RGB values
      */
-    @Override
-    public List<OneField> getFields(OneRow row) throws IOException {
-        URI uri = (URI) row.getKey();
-        Path path = Paths.get(uri.getPath());
-
-        List<OneField> payload = new ArrayList<>();
-        payload.add(new OneField(0, uri.toString()));
-        payload.add(new OneField(0, path.getParent().getFileName().toString()));
-        payload.add(new OneField(0, path.getFileName().toString()));
-
-        StringBuilder sb = new StringBuilder();
-        Object data = row.getData();
-        if (data instanceof InputStream) {
-            InputStream stream = (InputStream) row.getData();
-            processImage(sb, ImageIO.read(stream));
-            stream.close();
-        } else if (data instanceof ArrayList) {
-            int cnt = 0;
-            final ArrayList<InputStream> inputStreams = (ArrayList) data;
-            sb.append("{");
-            for (InputStream stream : inputStreams) {
-                processImage(sb, ImageIO.read(stream));
-                stream.close();
-                if (++cnt == inputStreams.size()) {
-                    continue;
-                }
-                sb.append(",");
-            }
-            sb.append("}");
-        } else {
-            return null;
-        }
-
-        payload.add(new OneField(0, sb.toString()));
-        return payload;
-    }
-
     @Override
     public List<OneField> startBatch(OneRow row) {
         URI uri = (URI) row.getKey();
@@ -89,34 +58,50 @@ public class ImageResolver extends BasePlugin implements BatchResolver {
     @Override
     public byte[] getNextBatchedItem(OneRow row) {
         if (currentImage == inputStreams.size()) {
-            return null;
+            return null; // already sent over the last image
         }
-        StringBuilder sb = new StringBuilder(1024 * 3);
-        if (currentImage == 0) {
-            sb.append(",\"{");
-        }
-        InputStream stream = inputStreams.get(currentImage++);
+        StringBuilder sb;
+        InputStream stream = inputStreams.get(currentImage);
         try {
-            processImage(sb, ImageIO.read(stream));
+            BufferedImage image = ImageIO.read(stream);
+            int h = image.getHeight();
+            int w = image.getWidth();
+            // avoid arrayCopy() in sb.append() by pre-calculating max image size
+            sb = new StringBuilder(
+                    w * h * 13 +  // each RGB is at most 13 chars: {255,255,255}
+                            (w - 1) * h + // commas separating RGBs
+                            h * 2 +       // curly braces surrounding each row of RGBs
+                            h - 1 +       // commas separating each row
+                            2 +           // outer curly braces for the image
+                            2 +           // any combination of image-separating commas and/or outer braces
+                            1 +           // opening CSV comma
+                            2 +           // double quote chars
+                            1             // newline
+            );
+            LOG.debug("Image length: {}, cap: {}", sb.length(), sb.capacity());
+            if (currentImage++ == 0) {
+                sb.append(",\"{");
+            }
+            processImage(sb, image, h, w);
             stream.close();
         } catch (IOException e) {
             LOG.info(e.getMessage());
+            return null;
         }
         if (currentImage != inputStreams.size()) {
             sb.append(",");
         } else {
             sb.append("}\"\n");
         }
+        LOG.debug("Image length: {}, cap: {}", sb.length(), sb.capacity());
 
         return sb.toString().getBytes();
     }
 
-    private void processImage(StringBuilder sb, BufferedImage image) {
+    private void processImage(StringBuilder sb, BufferedImage image, int h, int w) {
         if (image == null) {
             return;
         }
-        int w = image.getWidth();
-        int h = image.getHeight();
 
         LOG.debug("Image size {}w {}h", w, h);
 
@@ -125,7 +110,7 @@ public class ImageResolver extends BasePlugin implements BatchResolver {
             sb.append("{");
             for (int j = 0; j < w; j++) {
                 int pixel = image.getRGB(j, i);
-                sb.append(reds[(pixel >> 16) & 0xff] + greens[(pixel >> 8) & 0xff] + blues[pixel & 0xff] + ",");
+                sb.append(r[(pixel >> 16) & 0xff]).append(g[(pixel >> 8) & 0xff]).append(b[pixel & 0xff]).append(",");
             }
             sb.setLength(sb.length() - 1);
             sb.append("},");
