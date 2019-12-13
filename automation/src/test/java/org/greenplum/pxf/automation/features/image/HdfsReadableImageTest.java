270eb97e0a2cd1ca4312059f3c32fc87b1ea3fdf
Oliver Albertini <oalbertini@pivotal.io>
2019-12-12 21:48:46 -0800

    Refactor automation
    Authored-by: Oliver Albertini <oalbertini@pivotal.io>

--- automation/src/test/java/org/greenplum/pxf/automation/features/image/HdfsReadableImageTest.java
+++ automation/src/test/java/org/greenplum/pxf/automation/features/image/HdfsReadableImageTest.java
@@ -8,25 +8,21 @@ import org.testng.annotations.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 public class HdfsReadableImageTest extends BaseFeature {
+    private static final int NUM_IMAGES = 5;
     private String hdfsPath;
-    BufferedImage[] bufferedImages;
-    File[] filesToDelete;
-    private static int w = 256;
-    private static int h = 128;
-    private String publicStage = "/tmp/publicstage/pxf";
-    private File publicStageDir;
-    private StringBuilder[] images;
-
+    private File[] imageFiles;
+    private StringBuilder[] imagesPostgresArrays;
     private Table compareTable;
+    private String[] fullPaths;
+    private String[] directories;
+    private String[] names;
+
 
     @Override
     public void beforeClass() throws Exception {
@@ -37,13 +33,18 @@ public class HdfsReadableImageTest extends BaseFeature {
     }
 
     private void prepareData() throws Exception {
-        bufferedImages = new BufferedImage[5];
-        filesToDelete = new File[5];
-        for (int i = 0; i < bufferedImages.length; i++) {
+        BufferedImage[] bufferedImages = new BufferedImage[NUM_IMAGES];
+        imageFiles = new File[NUM_IMAGES];
+        fullPaths = new String[NUM_IMAGES];
+        directories = new String[NUM_IMAGES];
+        names = new String[NUM_IMAGES];
+        int w = 256;
+        int h = 128;
+        for (int i = 0; i < NUM_IMAGES; i++) {
             bufferedImages[i] = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
         }
-        images = new StringBuilder[5];
-        appendToImages(images, "{{", 0);
+        imagesPostgresArrays = new StringBuilder[NUM_IMAGES];
+        appendToImages(imagesPostgresArrays, "{{", 0);
 
         Map<String, Integer> maskOffsets = new HashMap<String, Integer>() {{
             put("r", 16);
@@ -52,34 +53,48 @@ public class HdfsReadableImageTest extends BaseFeature {
         }};
 
         Random rand = new Random();
-        for (int j = 0; j < bufferedImages.length; j++) {
+        for (int j = 0; j < NUM_IMAGES; j++) {
             for (int i = 0; i < w * h; i++) {
-                final int r = rand.nextInt(255) << maskOffsets.get("r");
-                final int g = rand.nextInt(255) << maskOffsets.get("g");
-                final int b = rand.nextInt(255) << maskOffsets.get("b");
+                final int r = rand.nextInt(256) << maskOffsets.get("r");
+                final int g = rand.nextInt(256) << maskOffsets.get("g");
+                final int b = rand.nextInt(256) << maskOffsets.get("b");
                 bufferedImages[j].setRGB(i % w, i / w, r + g + b);
-                images[j].append("{").append(r >> maskOffsets.get("r")).append(",").append(g >> maskOffsets.get("g")).append(",").append(b >> maskOffsets.get("b")).append("},");
+                imagesPostgresArrays[j]
+                        .append("{")
+                        .append(r >> maskOffsets.get("r"))
+                        .append(",")
+                        .append(g >> maskOffsets.get("g"))
+                        .append(",")
+                        .append(b >> maskOffsets.get("b"))
+                        .append("},");
                 if ((i + 1) % w == 0) {
-                    appendToImage(images[j], "},{", 1);
+                    appendToImage(imagesPostgresArrays[j], "},{", 1);
                 }
             }
         }
+        appendToImages(imagesPostgresArrays, "}", 2);
 
-        appendToImages(images, "}", 2);
-        publicStageDir = new File(publicStage);
-        if (!publicStageDir.exists()) {
-            if (!publicStageDir.mkdirs()) {
-                throw new RuntimeException(String.format("Could not create %s", publicStage));
-            }
+        String publicStage = "/tmp/publicstage/pxf";
+        createDirectory(publicStage);
 
-        }
         int cnt = 0;
         for (BufferedImage bi : bufferedImages) {
-            final String fileName = String.format("%d.png", cnt);
-            filesToDelete[cnt] = new File(publicStage + "/" + fileName);
-            ImageIO.write(bi, "png", filesToDelete[cnt]);
-            hdfs.copyFromLocal(filesToDelete[cnt].toString(), hdfsPath + "/" + fileName);
+            names[cnt] = String.format("%d.png", cnt);
+            imageFiles[cnt] = new File(publicStage + "/" + names[cnt]);
+            ImageIO.write(bi, "png", imageFiles[cnt]);
+            fullPaths[cnt] = hdfsPath + "/" + names[cnt];
+            hdfs.copyFromLocal(imageFiles[cnt].toString(), fullPaths[cnt]);
+            directories[cnt] = "readableImage";
             cnt++;
+        }
+    }
+
+    private void createDirectory(String dir) {
+        File publicStageDir = new File(dir);
+        if (!publicStageDir.exists()) {
+            if (!publicStageDir.mkdirs()) {
+                throw new RuntimeException(String.format("Could not create %s", dir));
+            }
         }
     }
 
@@ -108,23 +123,30 @@ public class HdfsReadableImageTest extends BaseFeature {
         exTable = new ReadableExternalTable("image_test", null, "", "CSV");
         exTable.setHost(pxfHost);
         exTable.setPort(pxfPort);
-        exTable.setFields(new String[]{"fullpaths TEXT[]", "directories TEXT[]", "names TEXT[]", "images INT[]"});
+        final String[] imageTableFieldsList = {"fullpaths TEXT[]", "directories TEXT[]", "names TEXT[]", "images INT[]"};
+        exTable.setFields(imageTableFieldsList);
         exTable.setPath(hdfsPath + "/*.png");
         exTable.setProfile(ProtocolUtils.getProtocol().value() + ":image");
-        compareTable = new Table("compare_table", new String[]{"id INT", "images INT[]"});
-        compareTable.setDistributionFields(new String[]{"id"});
+        compareTable = new Table("compare_table", imageTableFieldsList);
+        compareTable.setDistributionFields(new String[]{"names"});
     }
 
     /**
      * Read a single image from HDFS
      */
     @Test(groups = {"features", "gpdb", "hcfs", "security"})
-    public void singleImage() throws Exception {
+    public void batchSize1() throws Exception {
         exTable.setName("image_test_batchsize_1");
         compareTable.setName("compare_table_batchsize_1");
         int cnt = 0;
-        for (StringBuilder image : images) {
-            compareTable.addRow(new String[]{String.valueOf(cnt++), "'{" + image + "}'"});
+        for (StringBuilder image : imagesPostgresArrays) {
+            compareTable.addRow(new String[]{
+                    "'{" + fullPaths[cnt] + "}'",
+                    "'{" + directories[cnt] + "}'",
+                    "'{" + names[cnt] + "}'",
+                    "'{" + image + "}'"
+            });
+            cnt++;
         }
 
         gpdb.createTableAndVerify(exTable);
@@ -142,7 +164,7 @@ public class HdfsReadableImageTest extends BaseFeature {
         if (ProtocolUtils.getPxfTestDebug().equals("true")) {
             return;
         }
-        for (File fileToDelete : filesToDelete) {
+        for (File fileToDelete : imageFiles) {
             if (!fileToDelete.delete()) {
                 throw new RuntimeException(String.format("Could not delete %s", fileToDelete));
             }
