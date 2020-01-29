cb0262b2b6524f428953bd25097ef96c8f0d1c7a
Oliver Albertini <oalbertini@pivotal.io>
2020-01-28 18:07:37 -0800

    Add print statement
    Authored-by: Oliver Albertini <oalbertini@pivotal.io>

--- concourse/scripts/compile_pxf.bash
+++ concourse/scripts/compile_pxf.bash
@@ -6,8 +6,10 @@ GPHOME=/usr/local/greenplum-db-devel
 PXF_ARTIFACTS_DIR=${PWD}/${OUTPUT_ARTIFACT_DIR}
 
 # use a login shell for setting environment
+getent hosts
 bash --login -c "
 	export PXF_HOME=${GPHOME}/pxf BUILD_NUMBER=${TARGET_OS}
+	getent hosts
 	make -C '${PWD}/pxf_src' test install
 "
 
