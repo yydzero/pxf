107670a8026a53f13f4dabd93304d22bc23e2286
Francisco Guerrero <aguerrero@pivotal.io>
2020-02-19 07:58:04 -0800

    add name to thread factory, error -> debug

--- server/pxf-api/src/main/java/org/greenplum/pxf/api/ExecutorServiceProvider.java
+++ server/pxf-api/src/main/java/org/greenplum/pxf/api/ExecutorServiceProvider.java
@@ -1,18 +1,21 @@
 package org.greenplum.pxf.api;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
 
 public class ExecutorServiceProvider {
 
     // TODO: pick a better executor thread pool, maybe some elastic threadpool
     // TODO: maybe 10X number of cores
 
+    public static final ThreadFactory NAMED_THREAD_FACTORY =
+            new ThreadFactoryBuilder().setNameFormat("pxf-worker-%d").build();
+
     public static final ExecutorService EXECUTOR_SERVICE =
             new ThreadPoolExecutor(16, 16, 1,
-                    TimeUnit.SECONDS, new LinkedBlockingDeque<>(10), new ThreadPoolExecutor.CallerRunsPolicy());
+                    TimeUnit.SECONDS, new LinkedBlockingDeque<>(10),
+                    NAMED_THREAD_FACTORY, new ThreadPoolExecutor.CallerRunsPolicy());
 
     public static ExecutorService get() {
         // TODO: implement executor service per server / read Configuration here as well
--- server/pxf-api/src/main/java/org/greenplum/pxf/api/model/TaskProducer.java
+++ server/pxf-api/src/main/java/org/greenplum/pxf/api/model/TaskProducer.java
@@ -22,35 +22,34 @@ public class TaskProducer<T> extends Thread {
         this.querySession = requireNonNull(querySession, "querySession cannot be null");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void run() {
         try {
             BlockingDeque<Iterator<QuerySplit>> queue = querySession.getSplitIteratorQueue();
             Iterator<QuerySplit> iterator;
 
-            LOG.error("fetching iterator");
+            LOG.debug("fetching QuerySplit iterator");
             /* Wait for 11 seconds until segments have registered their iterators */
             while (querySession.isActive() && (iterator = queue.poll(11, TimeUnit.SECONDS)) != null) {
-                LOG.error("iterator fetched");
+                LOG.debug("new QuerySplit iterator fetched");
                 while (iterator.hasNext() && querySession.isActive()) {
-
                     QuerySplit split = iterator.next();
 
                     LOG.debug("Submitting {} to the pool for query {}", split, querySession);
 
                     executor.submit(new TupleReaderTask<>(split, querySession));
-
                     // Increase the number of jobs submitted to the executor
                     querySession.registerTask();
-
                     // Need to mark the session as started producing
                     querySession.markAsStartedProducing();
                 }
             }
-            LOG.error("task producer completed");
-
             /* Edge case when there are no splits or an error occurs */
             querySession.markAsStartedProducing();
+            LOG.debug("task producer completed");
         } catch (Exception ex) {
             querySession.errorQuery();
             throw new RuntimeException(ex);
